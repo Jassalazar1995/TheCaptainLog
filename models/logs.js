3950f77bb4b025204ddcedf96186f85f361aef39
Jose Salazar <jsalazar@pneumarespiratory.com>
2024-01-22 16:35:40 -0500

    Upgraded create route by making a model of the schema, then created a document in the database in the server. Made the create route async and used the create method.

--- models/logs.js
+++ models/logs.js
@@ -5,5 +5,9 @@ const Schema = mongoose.Schema
 const entrySchema = new Schema({
     title:{type:String, required:true},
     entry:{type:String, required:true},
-    shipIsBroken:{type:Boolean, require:true}
-})
+    shipIsBroken:{type:Boolean, require:true, default: true}
+},  { timestamps: true })
+
+// First arg is collection name
+const log = mongoose.model("posts",entrySchema)
+module.exports = log
--- server.js
+++ server.js
@@ -1,6 +1,11 @@
+require('dotenv').config()
+const Logs = require('./models/logs');
+
 const express = require('express');
 const app = express();
 const PORT = 5000;
+
+const mongoConfig = require('./config')
 
 const jsxEngine = require('jsx-view-engine');
 app.set('view engine', 'jsx');
@@ -15,13 +20,15 @@ app.get('/', (req, res) => {
 });
 
 // Create route
-app.post('/submitted', (req, res) => {
+app.post('/submitted', async (req, res) => {
     // Convert checkbox value to boolean
     req.body.Broken = req.body.Broken === 'on' ? false : true;
     console.log(req.body);
+    const logs = await Logs.create(req.body)
     res.send(req.body); // Send back the received data for testing
 });
 
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`);
+    mongoConfig()
 });
